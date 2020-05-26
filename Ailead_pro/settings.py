ad1bf166d7685b3930d42b7d3180e1c814d2f315
kizzteee <58609038+kizzteee@users.noreply.github.com>
2020-05-26 07:49:48 +0100

    initial commit

--- Ailead_pro/settings.py
+++ Ailead_pro/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django-heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -141,6 +140,3 @@ EMAIL_HOST_PASSWORD = 'excellen'
 
 ADMINS = [('Ikeoluwa', EMAIL_HOST)]
 MANAGERS = ADMINS
-
-
-django_heroku.settings(locals())
